4a6fd5b96b38bf5bc428eb0b968f7109979e7bad
25th-Night <brown_eyed87@naver.com>
2023-09-05 21:19:16 +0900

    staging 모듈 : network 리소스 생성

--- infra/NCP/stage/staging/main.tf
+++ infra/NCP/stage/staging/main.tf
@@ -10,7 +10,25 @@ terraform {
 provider "ncloud" {
   access_key  = var.ncp_access_key
   secret_key  = var.ncp_secret_key
-  region      = KR
-  site        = public
+  region      = "KR"
+  site        = "public"
   support_vpc = true
+}
+
+locals {
+  env           = "staging"
+  lb_name       = "be"
+  subnet_netnum = 1
+  subnet_type   = "PUBLIC"
+}
+
+module "network" {
+  source = "../../modules/network"
+
+  ncp_access_key = var.ncp_access_key
+  ncp_secret_key = var.ncp_secret_key
+  name           = local.lb_name
+  env            = local.env
+  subnet_netnum  = local.subnet_netnum
+  subnet_type    = local.subnet_type
 }
