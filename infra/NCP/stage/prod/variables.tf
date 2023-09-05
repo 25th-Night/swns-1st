d013d39c3a6a0e645236235fe99e172951114cbc
25th-Night <brown_eyed87@naver.com>
2023-09-05 23:36:27 +0900

    prod 모듈 : 모든 리소스 생성

---
+++ infra/NCP/stage/prod/variables.tf
@@ -0,0 +1,69 @@
+variable "ncp_access_key" {
+  type = string
+}
+
+variable "ncp_secret_key" {
+  type = string
+}
+
+variable "username" {
+  type      = string
+  sensitive = true
+}
+
+
+variable "password" {
+  type      = string
+  sensitive = true
+}
+
+
+variable "postgres_db" {
+  type = string
+}
+
+
+variable "postgres_user" {
+  type      = string
+  sensitive = true
+}
+
+
+variable "postgres_password" {
+  type      = string
+  sensitive = true
+}
+
+
+variable "postgres_volume" {
+  type = string
+}
+
+
+variable "db_container_name" {
+  type = string
+}
+
+variable "django_settings_module" {
+  type = string
+}
+
+variable "django_secret_key" {
+  type = string
+}
+
+variable "django_container_name" {
+  type = string
+}
+
+variable "ncr_host" {
+  type = string
+}
+
+variable "ncr_image" {
+  type = string
+}
+
+variable "ncp_lb_domain" {
+  type = string
+}
