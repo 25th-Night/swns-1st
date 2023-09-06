6d0c7c6633c8de7d019f02147266a6b2b2f714e9
25th-Night <brown_eyed87@naver.com>
2023-09-07 02:38:10 +0900

    posts/tests: slug 정상 여부 테스트

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.utils import timezone
+from users.models import User
+from .models import Post
 
-# Create your tests here.
+
+class PostModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="test@test.com",
+            fullname="테스터",
+            phone="010-1111-1111",
+            password="1234",
+        )
+        self.post = Post.objects.create(
+            title="test post title",
+            body="test post body",
+            author=self.user,
+            status=Post.StatusChoices.DRAFT,
+            publish=timezone.now(),
+        )
+
+    def test_save_method(self):
+        expected_result = "test-post-title"
+        self.assertEqual(self.post.slug, expected_result)
+
+    def tearDown(self):
+        self.post.delete()
+        self.user.delete()
