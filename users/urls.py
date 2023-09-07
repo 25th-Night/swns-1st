2d132b03b80f4554df4315670111527947b918e4
25th-Night <brown_eyed87@naver.com>
2023-09-08 02:01:04 +0900

    users/urls: token url 경로 수정 및 UserViewSet url 추가

--- users/urls.py
+++ users/urls.py
@@ -4,15 +4,16 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from users.views import SignUpView, LoginView
+from users.views import SignUpView, LoginView, UserViewSet
 
 router = DefaultRouter()
+router.register("", UserViewSet, basename="user")
 # router.register("path명", ViewSet클래스, basename="basename지정")
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path("/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
+    path("", include(router.urls)),
 ]
