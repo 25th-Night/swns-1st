55ee358502aa1f1eafe3027958c3325bed9348f5
25th-Night <brown_eyed87@naver.com>
2023-09-08 02:00:32 +0900

    users/views: UserFilter, UserViewSet 추가

--- users/views.py
+++ users/views.py
@@ -4,16 +4,21 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
+from rest_framework.request import Request
 
-from users.serializers import SignUpSeiralizer, LoginSeiralizer
+from django_filters import rest_framework as filters
+
+from users.models import User
+from users.permissions import CustomReadOnly
+from users.serializers import SignUpSeiralizer, LoginSeiralizer, UserSerializer
 
 
 class SignUpView(APIView):
     serializer_class = SignUpSeiralizer
     permission_classes = [AllowAny]
 
-    def post(self, request):
+    def post(self, request: Request):
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
@@ -38,7 +43,7 @@ class LoginView(APIView):
     serializer_class = LoginSeiralizer
     permission_classes = [AllowAny]
 
-    def post(self, request):
+    def post(self, request: Request):
         email = request.data.get("email")
         password = request.data.get("password")
         user = authenticate(email=email, password=password)
@@ -57,4 +62,34 @@ class LoginView(APIView):
                 },
                 status=status.HTTP_200_OK,
             )
-        return Response({"message": "login failed"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"message": "login failed"}, status=status.HTTP_401_UNAUTHORIZED
+        )
+
+
+class UserFilter(filters.FilterSet):
+    email = filters.CharFilter(field_name="email", lookup_expr="icontains")
+    fullname = filters.CharFilter(field_name="fullname", lookup_expr="icontains")
+    phone = filters.CharFilter(field_name="phone", lookup_expr="icontains")
+
+    class Meta:
+        model = User
+        fields = ["email", "fullname", "phone", "is_active"]
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [CustomReadOnly]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filterset_class = UserFilter
+
+    def list(self, request: Request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request: Request, *args, **kwargs):
+        return Response(
+            data={"message": "Create operation is not supported"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
