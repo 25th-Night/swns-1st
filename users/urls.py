73f9ed15579ba35ecafa99a4860a48190bdb5925
25th-Night <brown_eyed87@naver.com>
2023-09-07 20:10:58 +0900

    users/urls: SignUpView를 url에 등록

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from users.views import SignUpView
+
 router = DefaultRouter()
 # router.register("path명", ViewSet클래스, basename="basename지정")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("signup/", SignUpView.as_view(), name="signup"),
 ]
