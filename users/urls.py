544303ac1a9619115b74f4c0bcdbf48b69d7270c
25th-Night <brown_eyed87@naver.com>
2023-09-07 22:07:43 +0900

    users/urls: token 발급 관련 url 추가

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from users.views import SignUpView
 
 router = DefaultRouter()
@@ -8,5 +11,7 @@ router = DefaultRouter()
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("signup/", SignUpView.as_view(), name="signup"),
 ]
