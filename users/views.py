6508c555a381dfdb7866b6aecc09994101c45aff
25th-Night <brown_eyed87@naver.com>
2023-09-07 20:10:40 +0900

    users/views: SIgnUpView 추가

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from users.serializers import SignUpSeiralizer
+
+
+class SignUpView(APIView):
+    serializer_class = SignUpSeiralizer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            token = RefreshToken.for_user(user)
+            refresh = str(token)
+            access = str(token.access_token)
+
+            return Response(
+                {"access": access, "refresh": refresh}, status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
