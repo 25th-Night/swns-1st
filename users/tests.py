75a1b29c8801ecb6a911fb0ead12cd32859ddc94
25th-Night <brown_eyed87@naver.com>
2023-09-08 13:04:26 +0900

    users/tests: Profile 관련 테스트 추가

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.urls import reverse
@@ -5,6 +7,8 @@ from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from rest_framework.response import Response
+
+from users.models import Profile
 
 
 class SignUpViewTest(APITestCase):
@@ -141,6 +145,9 @@ class UserViewSetTest(APITestCase):
             phone="0133333333",
             password="password",
         )
+        cls.user01_profile = Profile.objects.create(
+            user=cls.user01, nickname="TestUser", birthday=datetime.now().date()
+        )
 
     def test_list_data(self):
         test_url = reverse("user-list")
@@ -178,5 +185,64 @@ class UserViewSetTest(APITestCase):
         client = APIClient()
         client.force_authenticate(user=self.user01)
         res: Response = client.get(test_url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_profile_with_no_login(self):
+        test_url = reverse("user-profile", args=[self.user01.pk])
+        client = APIClient()
+        res: Response = client.get(test_url)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_profile_with_login(self):
+        test_url = reverse("user-profile", args=[self.user01.pk])
+        client = APIClient()
+        client.force_authenticate(user=self.user01)
+        res: Response = client.get(test_url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data.get("id"), self.user01_profile.id)
+
+
+class ProfileViewSetTest(APITestCase):
+    # Set up
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_model = Profile
+        cls.user_model = get_user_model()
+        cls.test_url = reverse("profile-list")
+        cls.user01 = cls.user_model.objects.create(
+            email="user01@example.com",
+            fullname="User01",
+            phone="01000000000",
+            password="password",
+        )
+        cls.profile = cls.test_model.objects.create(
+            user=cls.user01, nickname="TestUser01", birthday=datetime.now().date()
+        )
+        cls.user02 = cls.user_model.objects.create(
+            email="user02@example.com",
+            fullname="User02",
+            phone="01000000001",
+            password="password",
+        )
+        cls.user02_profile_data = {
+            "user": cls.user02.id,
+            "nickname": "TestUser02",
+            "birthday": datetime.now().date(),
+        }
+
+    def test_create_data(self):
+        client = APIClient()
+        client.force_authenticate(user=self.user02)
+        res: Response = client.post(self.test_url, self.user02_profile_data)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(self.test_model.objects.count(), 2)
+
+    def test_list_data_with_no_login(self):
+        client = APIClient()
+        res: Response = client.get(self.test_url)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
