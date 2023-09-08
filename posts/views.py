a4c7664f588e5ec16eec0070661ad05977bd7a1a
25th-Night <brown_eyed87@naver.com>
2023-09-08 22:29:52 +0900

    posts/views: PostViewSet 추가

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from rest_framework.request import Request
 
-# Create your views here.
+from posts.models import Post
+from posts.permissions import PostCustomReadOnly
+from posts.serializers import PostSerializer
+from posts.filters import PostFilter
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [PostCustomReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filterset_class = PostFilter
+
+    def list(self, request: Request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
