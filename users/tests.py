47720b1b2c1103f186931e0d1dee993420fd4e22
25th-Night <brown_eyed87@naver.com>
2023-09-08 18:25:22 +0900

    users/tests: follow 관련 테스트 추가

--- users/tests.py
+++ users/tests.py
@@ -1,14 +1,13 @@
 from datetime import datetime
 
 from django.contrib.auth import get_user_model
-from django.http import HttpResponse
 from django.urls import reverse
 
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from rest_framework.response import Response
 
-from users.models import Profile
+from users.models import Follow, Profile
 
 
 class SignUpViewTest(APITestCase):
@@ -148,6 +147,15 @@ class UserViewSetTest(APITestCase):
         cls.user01_profile = Profile.objects.create(
             user=cls.user01, nickname="TestUser", birthday=datetime.now().date()
         )
+        cls.follow01 = Follow.objects.create(
+            user_from=cls.admin_user, user_to=cls.user01
+        )
+        cls.follow02 = Follow.objects.create(
+            user_from=cls.admin_user, user_to=cls.user02
+        )
+        cls.follow03 = Follow.objects.create(
+            user_from=cls.user03, user_to=cls.admin_user
+        )
 
     def test_list_data(self):
         test_url = reverse("user-list")
@@ -204,6 +212,44 @@ class UserViewSetTest(APITestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data.get("id"), self.user01_profile.id)
 
+    def test_following_data(self):
+        test_url = reverse("user-following", args=[self.admin_user.pk])
+        client = APIClient()
+        client.force_authenticate(user=self.admin_user)
+        res: Response = client.get(test_url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            Follow.objects.filter(user_from=self.admin_user).count(), len(res.data)
+        )
+
+    def test_follower_data(self):
+        test_url = reverse("user-follower", args=[self.admin_user.pk])
+        client = APIClient()
+        client.force_authenticate(user=self.admin_user)
+        res: Response = client.get(test_url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            Follow.objects.filter(user_to=self.admin_user).count(), len(res.data)
+        )
+
+    def test_follow_data(self):
+        test_url = reverse("user-follow", args=[self.admin_user.pk, self.user03.pk])
+        client = APIClient()
+        client.force_authenticate(user=self.admin_user)
+        res: Response = client.post(test_url)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_unfollow_data(self):
+        test_url = reverse("user-follow", args=[self.admin_user.pk, self.user01.pk])
+        client = APIClient()
+        client.force_authenticate(user=self.admin_user)
+        res: Response = client.post(test_url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+
 
 class ProfileViewSetTest(APITestCase):
     # Set up
@@ -246,3 +292,52 @@ class ProfileViewSetTest(APITestCase):
         res: Response = client.get(self.test_url)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+class FollowViewSetTest(APITestCase):
+    # Set up
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_model = Follow
+        cls.user_model = get_user_model()
+        cls.test_url = reverse("follow-list")
+        cls.user01 = cls.user_model.objects.create(
+            email="user01@example.com",
+            fullname="User01",
+            phone="01111111111",
+            password="password",
+        )
+        cls.user02 = cls.user_model.objects.create(
+            email="user02@example.com",
+            fullname="User02",
+            phone="01222222222",
+            password="password",
+        )
+        cls.user03 = cls.user_model.objects.create(
+            email="user03@example.com",
+            fullname="User03",
+            phone="0133333333",
+            password="password",
+        )
+        cls.follow01 = cls.test_model.objects.create(
+            user_from=cls.user01, user_to=cls.user02
+        )
+        cls.follow02 = cls.test_model.objects.create(
+            user_from=cls.user02, user_to=cls.user03
+        )
+        cls.follow_data01 = {"user_from": cls.user01.id, "user_to": cls.user03.id}
+
+    def test_create_data(self):
+        client = APIClient()
+        client.force_authenticate(user=self.user01)
+        res: Response = client.post(self.test_url, self.follow_data01)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(self.test_model.objects.count(), 3)
+
+    def test_list_data_with_no_login(self):
+        client = APIClient()
+        res: Response = client.get(self.test_url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.test_model.objects.count(), 2)
