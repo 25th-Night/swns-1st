7023190a028af470379cb786cdb85c34e1d51c43
25th-Night <brown_eyed87@naver.com>
2023-09-10 11:17:48 +0900

    common/utils: Image 클래스 추가

---
+++ common/utils.py
@@ -0,0 +1,37 @@
+import uuid
+from datetime import datetime
+
+from django.conf import settings
+from django.core.files.base import File
+
+from boto3 import client
+
+
+class Image:
+    def __init__(self, image: File):
+        self.file = image.file
+        self.name = image.name
+        self.id = str(uuid.uuid4())
+        self.ext = self.name.split(".")[-1]
+        self.directory = datetime.now().date()
+        self.filename = f"{self.directory}/{self.id}.{self.ext}"
+        self.url = None
+        self.access_key = settings.NCP_ACCESS_KEY
+        self.secret_key = settings.NCP_SECRET_KEY
+        self.endpoint_url = settings.S3_ENDPOINT_URL
+        self.bucket_name = settings.S3_BUCKET_NAME
+        self.client: client = client(
+            service_name="s3",
+            endpoint_url=self.endpoint_url,
+            aws_access_key_id=self.access_key,
+            aws_secret_access_key=self.secret_key,
+        )
+
+    def s3_upload(self):
+        self.client.upload_fileobj(self.file, self.bucket_name, self.filename)
+        self.url = f"{self.endpoint_url}/{self.bucket_name}/{self.filename}"
+
+    def set_public_in_s3(self):
+        response = self.client.put_object_acl(
+            Bucket=self.bucket_name, Key=self.filename, ACL="public-read"
+        )
